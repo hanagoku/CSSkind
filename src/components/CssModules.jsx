8ab74f07ce494e63eef3951862058013b4e62657
hanagoku <108511558+hanagoku@users.noreply.github.com>
2022-07-12 18:51:42 +0900

    20220712   様々なCSS適用方法 1851

---
+++ src/components/CssModules.jsx
@@ -0,0 +1,9 @@
+import classes from "./CssModules.module.scss";
+export const CssModules = () => {
+  return (
+    <div>
+      <p>-CssModules-</p>
+      <button>FIGHT!!</button>
+    </div>
+  );
+};
